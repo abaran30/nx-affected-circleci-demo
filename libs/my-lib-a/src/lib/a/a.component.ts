11d054645fb035217409ad44ce2e4215f10b77a2
Adrian Baran <adbaran@cisco.com>
2022-09-19 16:29:08 -0500

    fix: lint errors for my-lib-a

--- libs/my-lib-a/src/lib/a/a.component.ts
+++ libs/my-lib-a/src/lib/a/a.component.ts
@@ -1,12 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'nx-affected-circleci-demo-a',
   templateUrl: './a.component.html',
   styleUrls: ['./a.component.scss'],
 })
-export class AComponent implements OnInit {
-  constructor() {}
-
-  ngOnInit(): void {}
-}
+export class AComponent {}
