f7d9d59ce72e577466da1eb0009be5a4cb1d2cef
Adrian Baran <adbaran@cisco.com>
2023-05-15 16:23:47 -0500

    fix: lint

--- libs/my-lib-b/src/lib/b/b.component.ts
+++ libs/my-lib-b/src/lib/b/b.component.ts
@@ -1,12 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'nx-affected-circleci-demo-b',
   templateUrl: './b.component.html',
   styleUrls: ['./b.component.scss'],
 })
-export class BComponent implements OnInit {
-  constructor() {}
-
-  ngOnInit(): void {}
-}
+export class BComponent {}
--- libs/my-lib-c/src/lib/c/c.component.ts
+++ libs/my-lib-c/src/lib/c/c.component.ts
@@ -1,12 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'nx-affected-circleci-demo-c',
   templateUrl: './c.component.html',
   styleUrls: ['./c.component.scss'],
 })
-export class CComponent implements OnInit {
-  constructor() {}
-
-  ngOnInit(): void {}
-}
+export class CComponent {}
